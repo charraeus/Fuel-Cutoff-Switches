8a2591d0665c1030335963b7b010f0bce4ab7772
Christian Harraeus <christian@harraeus.de>
2022-12-19 15:50:49 +0100

    Add new functionality. Ready for testing.
    * Add the new functionality.
    * Make self-documenting code.
    * Add comments.
    * Test the functionality with gamepad-tester.com/
      Do no use Safari for this test, because it does not work there.
      Use Firefox.
    On branch development
    Your branch is up to date with 'origin/development'.
    
    Changes to be committed:
    	modified:   platformio.ini
    	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,35 +17,51 @@
  * Sinan has asked to be sent the new software version.  
  * Sinan <2_3c7aa44149d6c314fc53fb174d465ad073edbc46_1121328563999@convos.etsy.com>
  *  
- * ## Actual state
- * 
- * The fuel selector switches are passed as joystick buttons.  
- * The problem is that there is no mechanism in X-Plane to fire a trigger when a 
- * joystick button is released. Therefore the following software solution in the Arduino 
- * would be helpful:
+ *  ## Requirements
  *  
- * 1. when the fuel selector is switched on (i.e. switch up) - as before - a BTN is triggered, 
- * in my case these are BTN3 resp. BTN4.
- * 2. when switching off, another BTN is triggered, so for example BTN5 or BTN.
- * This way there are two joystick buttons that can be assigned a function in X-Plane.
+ *  1. Expose only 4 joystick buttons.
+ *  2. Expose these buttons as "Button 0" .. "Button 3".
+ *  3. Do not expose any axis'.
+ *  4. Do not expose any hat switch.
+ *  5. Switch the 4 joystick buttons according to the algorithm below.
+ *     * **Fuel Selector Switch #1** is set to *on*: show **joystick button 0 as pressed**, otherwise as depressed,
+ *     * **Fuel Selector Switch #1** is set to *off*: show **joystick button 1 as pressed**, otherwise as depressed,
+ *     * **Fuel Selector Switch #2** is set to *on*: show **joystick button 2 as pressed**, otherwise as depressed,
+ *     * **Fuel Selector Switch #2** is set to *off*: show **joystick button 3 as pressed**, otherwise as depressed.
+ *  6. The board in use is @em Sparkfun Micro Pro which is compatible to the Arduino Leonardo.
  *  
- *  ## Target state
- *
- *  **Algorithm:**  
- *  **As up to now:**
- *  * Joystick button is pressed --> the Arduino sets BTN3 resp. BTN4 to "active",
- *  * joystick button is released --> the Arduino sets BTN3 resp. BTN4 to "inactive".
+ *  ## Wireing
  *  
- *  **Additionally:**
- *  * Joystick button is pressed --> the Arduino sets BTN5 or BTN6 to "inactive".
- *  * Joystick button is released --> the Arduino sets BTN5 or BTN6 to "active".
- * 
- *  ## Next steps
+ *  1. **Fuel Selector Switch #1** is connected to **"Arduino Pin" 3** of Micro Pro board.
+ *  2. **Fuel Selector Switch #2** is connected to **"Arduino Pin" 2** of Micro Pro board. 
  *  
- *  @todo ~~1. Arduino-Leonardo Entwicklungsumgebung aufsetzen.~~
- *  @todo 2. Auf dem Leonardo befindliche Firmware auslesen und als Backup archivieren.
- *  @todo 3. Diese \.ino-Datei übersetzen und auf den Leonardo aufspielen und testen.
- *  @todo 4. Dann erst mit der Softwareänderung beginnen.
+ *  ## Algorithm for Button-Setting
+ *  
+ *  ### Initialisation
+ *  
+ *  Set joystick buttons according to position of the Fuel Selector Switches;
+ *  
+ *  ### Fuel Selector Switch State Change
+ *  
+ *  if a Fuel Selector Switch position is changed then
+ *      debounce new switch state (wait for max. 8 milliseconds until new state is stabilised);
+ *  endif
+ *  
+ *  **Fuel Selector Switch 1**  
+ *  ```
+ *  if Fuel Selector Switch is set to upper position (Arduino pin 3 = "on") then
+ *      set joystick button 1 to off;
+ *      set joystick button 0 to on;
+ *  endif
+ *  
+ *  if Fuel Selector Switch is set to down position (Arduino pin 3 = "off") then
+ *      set joystick button 0 to off;
+ *      set joystick button 1 to on;
+ *  endif
+ *  ```
+ *  
+ *  **Fuel Selector Switch 2**  
+ *  Analogous to Fuel Selector Switch 1, but with joystick buttons 2 and 3 and Pin 2.
  * 
  *  ## Further information
  *  * Used Joystick-Library: https://github.com/MHeironimus/ArduinoJoystickLibrary
@@ -56,57 +72,102 @@
 #include <Arduino.h>
 #include <Joystick.h>
 
-Joystick_ Joystick;
+/// @brief  Constants for fuel selector switch #1
+const int SELECTOR_SWITCH_1_PIN = 3;        /// Fuel Selector Switch 1 is connected to pin 3
+const uint8_t SELECTOR_SWITCH_1_ON_BUTTON = 0;  /// Button number to trigger when Switch 1 is set to on
+const uint8_t SELECTOR_SWITCH_1_OFF_BUTTON = 1; /// Button number to trigger when Switch 1 is set to off
 
-/// @brief setup des Arduino Leonardo
+/// @brief Constants for fuel selector switch #2
+const int SELECTOR_SWITCH_2_PIN = 2;        /// Fuel Selector Switch 2 is connected to pin 2
+const uint8_t SELECTOR_SWITCH_2_ON_BUTTON = 2;  /// Button number to trigger when Switch 2 is set to on
+const uint8_t SELECTOR_SWITCH_2_OFF_BUTTON = 3; /// Button number to trigger when Switch 2 is set to off
+
+/// @brief Array of pin numbers to which switches are connected.
+/// @note PINS must be filled with pin numbers in ascending order
+const int PIN_COUNT = 2;                    /// Number of active pins
+const int PINS[PIN_COUNT] = { SELECTOR_SWITCH_2_PIN,
+                              SELECTOR_SWITCH_1_PIN, 
+                            };   /// Pin numbers to which switches are connected
+
+/// @brief Array of last states of the pins
+uint8_t lastPinState[PIN_COUNT] = {0, 0};
+
+/// @brief Initialise Joystick object: Show only 4 buttons.
+Joystick_ Joystick = {
+                      JOYSTICK_DEFAULT_REPORT_ID,       //JOYSTICK_DEFAULT_REPORT_ID,
+                      JOYSTICK_TYPE_JOYSTICK,           // JOYSTICK_TYPE_JOYSTICK,
+                      PIN_COUNT * 2,    // buttonCount = 4,
+                      0,                // hatSwitchCount = 0,
+                      false,            // includeXAxis = false,
+                      false,            // includeYAxis = false,
+                      false,            // includeZAxis = false,
+                      false,            // includeRxAxis = false,
+                      false,            // includeRyAxis = false,
+                      false,            // includeRzAxis = false,
+                      false,            // includeRudder = false,
+                      false,            // includeThrottle = false,
+                      false,            // includeAccelerator = false,
+                      false,            // includeBrake = false,
+                      false             // includeSteering = false).
+};
+
+
+/// @brief Setup of @em Arduino @em Leonardo / @em Sparkfun @em Micro @em Pro
 void setup() {
-  // Initialize Button Pins
-  // for digitalRead pins A0-A5 = 18-23
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-  pinMode(15, INPUT_PULLUP);
-  pinMode(16, INPUT_PULLUP);
-  pinMode(18, INPUT_PULLUP);
-  pinMode(19, INPUT_PULLUP);
-  pinMode(20, INPUT_PULLUP);
-  pinMode(21, INPUT_PULLUP);
-  pinMode(22, INPUT_PULLUP);
+    /// Initialize Arduino pins
+    for (const auto &pin : PINS) {
+        pinMode(pin, INPUT_PULLUP);
+    }
 
-  // Initialize Joystick Library
-  Joystick.begin();
+    /// Initialize Joystick Library
+    Joystick.begin();
 }
 
-/// defining the total [#] of buttons and their pins
-const int ButtonToPinMap[22] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19,20,21,22};
-
-/// vorheriger Status des Buttons (?) --> ist noch zu verifizieren
-int lastButtonState[22] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-
-/// @brief loop des Arduino
+/// @brief Endless loop of @em Arduino @em Leonardo / @em Sparkfun @em Micro @em Pro.
+/// 
+/// Read state (on, off) for all defined pins and
+/// set the joystick buttons accordingly
 void loop() {
-
-  for (int index = 0; index < 22; index++)
-  {
-    int currentButtonState = !digitalRead(ButtonToPinMap[index]);
-    if (currentButtonState != lastButtonState[index])
-    {
-      Joystick.setButton(index, currentButtonState);
-      lastButtonState[index] = currentButtonState;
+    /// repeat for all defined pins
+    for (const auto &pin : PINS) {
+        /// read the pin status
+        int currentPinState = !digitalRead(PINS[pin - PIN_COUNT]);
+        /// Memorize the pin status in @em lastPinState and
+        /// set the joystick buttons
+        if (currentPinState != lastPinState[pin - PIN_COUNT]) {
+            lastPinState[pin - PIN_COUNT] = currentPinState;
+            switch (pin) {
+                case SELECTOR_SWITCH_1_PIN: {
+                    if (currentPinState == 1) {
+                        // Switch #1 is set to on (up position)
+                        Joystick.setButton(SELECTOR_SWITCH_1_OFF_BUTTON, 0);
+                        Joystick.setButton(SELECTOR_SWITCH_1_ON_BUTTON, 1);
+                    } else {
+                        // Switch #1 is set to off (down position)
+                        Joystick.setButton(SELECTOR_SWITCH_1_ON_BUTTON, 0);
+                        Joystick.setButton(SELECTOR_SWITCH_1_OFF_BUTTON, 1);
+                    }
+                    break;
+                }
+                case SELECTOR_SWITCH_2_PIN: {
+                    if (currentPinState == 1) {
+                        // Switch #2 is set to on (up position)
+                        Joystick.setButton(SELECTOR_SWITCH_2_OFF_BUTTON, 0);
+                        Joystick.setButton(SELECTOR_SWITCH_2_ON_BUTTON, 1);
+                    } else {
+                        // Switch #2 is set to off (down position)
+                        Joystick.setButton(SELECTOR_SWITCH_2_ON_BUTTON, 0);
+                        Joystick.setButton(SELECTOR_SWITCH_2_OFF_BUTTON, 1);
+                    }
+                    break;
+                }
+                default: {
+                    ; // this should never happen
+                }
+            }
+        }
     }
-  }
 
-  delay(50);
+    /// Wait 50 milliseconds before next loop
+    delay(50);
 }
